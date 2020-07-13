305e31bc12d19f6b3bad651c034dfc3f2d8352ba
godghdai <godghdai@gmail.com>
2020-07-13 18:35:48 +0800

    update

--- XiGua Video Downloader/common/ffmpegs.py
+++ XiGua Video Downloader/common/ffmpegs.py
@@ -12,7 +12,7 @@ class FFmpeg:
         if not os.path.exists(ffmpeg_path):
             raise FFmpegFileNotExistException("未找到ffmpeg.exe文件!!")
 
-        self.ffmpeg_path=ffmpeg_path
+        self.ffmpeg_path = ffmpeg_path
 
     def merge(self, video_path, audio_path, output_path):
         cmd = [self.ffmpeg_path, "-i", video_path, "-i", audio_path, "-c:v", "copy", "-c:a", "copy", output_path]
@@ -20,3 +20,8 @@ class FFmpeg:
         os.remove(video_path)
         os.remove(audio_path)
 
+    def to_mp3(self, audio_path, output_path):
+        cmd = [self.ffmpeg_path, "-i", audio_path, "-codec:a", "libmp3lame", "-b:a", "320k", "-f", "mp3", "-vn",
+               output_path]
+        subprocess.call(cmd, stdout=subprocess.PIPE)
+        os.remove(audio_path)
